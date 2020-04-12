daac54e7e35dc099219ed1e5947a77760fb77b7d
alistair garioch <alistairgarioch@gmail.com>
2020-04-12 23:01:48 +0100

    Updates

---
+++ precache-manifest.5ad7fa657cb76fd8432ce2364c7066f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cca8b149fb0a70e42004b81759d4bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358749fab8ffc79673a1",
+    "url": "/static/js/2.b3adc887.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b3adc887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c693f2dd108311c8b785",
+    "url": "/static/js/main.7ea8e4c9.chunk.js"
+  },
+  {
+    "revision": "2260a9a5fd360e28b0d7",
+    "url": "/static/js/runtime-main.88865c14.js"
+  }
+]);
